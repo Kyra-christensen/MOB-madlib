94e3b4392921642c58b0d5a629c5527afd610ff6
Kyra Christensen <christensen.kyral@gmail.com>
2021-10-08 14:40:45 -0700

    added const variable to app.js and style change

--- app.js
+++ app.js
@@ -7,3 +7,24 @@
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+const submitButton = document.getElementById('submit');
+const verb1 = document.getElementById('verb1');
+const verb2 = document.getElementById('verb2');
+const adj1 = document.getElementById('adj1');
+const adj2 = document.getElementById('adj2');
+const noun1 = document.getElementById('noun1');
+const noun2 = document.getElementById('noun2');
+const verb3 = document.getElementById('verb3');
+const verb4 = document.getElementById('verb4');
+const adj3 = document.getElementById('adj3');
+const verb5 = document.getElementById('verb5');
+const verb6 = document.getElementById('verb6');
+const noun3 = document.getElementById('noun3');
+const verb7 = document.getElementById('verb7');
+const verb8 = document.getElementById('verb8');
+
+
+submitButton.addEventListener('click', () => {
+
+
+})
